288d6ad13ff4b2db42b0797a938f194b9294f2e6
zotolee <akinayo711@gmail.com>
2022-01-27 05:59:49 -0800

    Saving to GitHub

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function ageInWeeks() {
     "use strict";
-    var birthYear = prompt('What year were you born friend?');
-    var ageInWeeks = (2022 - birthYear) * 52;
-    var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('You are ' + ageInWeeks + ' weeks old')
+    const birthYear = prompt('What year were you born friend?');
+    let ageInWeeks = (2022 - birthYear) * 52;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are ' + ageInWeeks + ' weeks old')
     h1.setAttribute('id', 'ageInWeeks');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
@@ -14,6 +14,6 @@ function reset() {
     document.getElementById('ageInWeeks').remove();
 }
 
-
+/* With more learning in Vanilla JS, i need to add an eventListener and some other stuffs to make it more responsive. Especially if someone press alphabet, it should be displaying 'put in a number and not alphabet' */
     
     
